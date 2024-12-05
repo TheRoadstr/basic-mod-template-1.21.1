b4bff38d9733b3df919df41fce256d1cfe053e81
TheRoadstr <vavedarozaga@gmail.com>
2024-12-05 20:50:41 +0800

    added pink garnet and raw pink garnet

---
+++ src/main/java/net/theroadstr/basicmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.theroadstr.basicmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.theroadstr.basicmod.BasicMod;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(BasicMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        BasicMod.LOGGER.info("Registering Mod Items for " + BasicMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PINK_GARNET);
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
+        });
+    }
+}
