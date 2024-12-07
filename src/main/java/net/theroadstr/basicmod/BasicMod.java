45a683fcb10aee0c4eedb3d9de4a8ab052a301e9
TheRoadstr <vavedarozaga@gmail.com>
2024-12-07 18:01:17 +0800

    added item groups

--- src/main/java/net/theroadstr/basicmod/BasicMod.java
+++ src/main/java/net/theroadstr/basicmod/BasicMod.java
@@ -3,6 +3,7 @@ package net.theroadstr.basicmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.theroadstr.basicmod.block.ModBlocks;
+import net.theroadstr.basicmod.item.ModItemGroups;
 import net.theroadstr.basicmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class BasicMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
